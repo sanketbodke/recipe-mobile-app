e4a2ed5038ed7f1cf7d27a0a5b0e14f62afb49f4
Shriyaaa.10 <114821672+shriyaExe@users.noreply.github.com>
2024-05-23 22:27:23 +0530

    update useThemeColor.js

--- hooks/useThemeColor.js
+++ hooks/useThemeColor.js
@@ -7,11 +7,8 @@ import { useColorScheme } from 'react-native';
 
 import { Colors } from '@/constants/Colors';
 
-export function useThemeColor(
-    props: { light ? : string;dark ? : string },
-    colorName: keyof typeof Colors.light & keyof typeof Colors.dark
-) {
-    const theme = useColorScheme() ? ? 'light';
+export function useThemeColor(props, colorName) {
+    const theme = useColorScheme() || 'light';
     const colorFromProps = props[theme];
 
     if (colorFromProps) {
