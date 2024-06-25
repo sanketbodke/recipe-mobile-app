8d8f58f7937cedf2f0e822757e3405a991b9aef5
sanket bodke <sanketbodke26@gmail.com>
2024-06-25 23:34:23 +0530

    add detailed recipe view on trending post click

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,18 +1,24 @@
-import { View, Text, FlatList, Image } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { router } from 'expo-router'
 
-const TrendingPost = ({data}) => {
-
+const TrendingPost = ({ data }) => {
     return (
         <FlatList
-            data={data}
+            data={data.reverse()}
             keyExtractor={(item) => item._id}
-            renderItem={({item}) => (
-                <Image
-                    source={{ uri: item.recipeImg }}
-                    className="w-52 h-72 rounded-[30px] mt-3 bg-white/10 mr-5"
-                    resizeMode='cover'
-                />
+            renderItem={({ item }) => (
+                <TouchableOpacity
+                    onPress={() => {
+                        router.push(item._id)
+                    }}
+                >
+                    <Image
+                        source={{ uri: item.recipeImg }}
+                        className="w-52 h-72 rounded-[30px] mt-3 bg-white/10 mr-5"
+                        resizeMode='cover'
+                    />
+                </TouchableOpacity>
             )}
             horizontal
         />
